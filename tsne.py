32f29a4c8e35849017fb09473891136a07fddce5
aashareddy14 <aashareddy14@gmail.com>
2021-04-14 22:28:02 -0400

    add functions to calculate Q matrix, KL divergence, and gradient of KL divergence

--- tsne.py
+++ tsne.py
@@ -1,5 +1,11 @@
-# load iris dataset, which will serve as test dataset for building the algorith
+# load libraries
 import numpy as np
+import pandas as pd
+from sklearn import datasets
+from sklearn.decomposition import PCA
+import math
+
+# load iris dataset, which will serve as test dataset for building the algorithm
 iris = datasets.load_iris()
 X = iris.data
 
@@ -77,12 +83,83 @@ def p_matrix(X, perplexity = 30.0, tol = 1e-5):
     return P
 
 
+# function to calculate Q_ij from Y in the new map
+def q_matrix(Y):
+    """
+    Finds Q_ij matrix
+
+    Inputs: Y- np.array of points in the new space
+
+    Output: Q-ij matrix
+    """
+
+    D = row_loop_dist(Y, squared_euc_dist)
+
+    sum_Qi = 0.0
+    for i in range(n):
+        for k in range(d):
+            if k != i:
+                Q[i, k] = math.exp(-D[i, k])
+                sum_Qi += Q[i, k]
+
+        sum_disti_Qi = 0.0
+
+        for k in range(d):
+            Q[i, k] /= sum_Qi
+            sum_disti_Qi += D[i, k] * Q[i, k]
+
+    return Q
+
+# Test Q matrix on X (even though input is normally Y)
+Q_ij = q_matrix(X)
+Q_ij
 
 
+# KL divergence function that we will minimize through Gradient Descent
+def KL(P, Q):
+    """
+    Finds KL divergence between P and Q matrices
 
-# Compute Q_ij matrix with set variance?
+    Inputs:
+        P - similarity matrix of points in the high-dimensional space
+        Q - similarity matrix of points in the low-dimensional map space
 
+    Output: KL divergence expression
+    """
 
+    kl = 0
+    for i in range(P.shape[0]):
+        for j in range(Q.shape[0]):
+            if Q[i, j] != 0 and P[i, j] != 0:
+                kl += P[i, j] * np.log(P[i, j] / Q[i, j])
+    return kl
+
+# test
+KL(P_ij, Q_ij)
+
+# Function to find gradient of KL divergence
+def grad_KL(P, Y):
+    """
+    Finds the gradient of the KL divergence between P and Q matrices expression
+
+    Inputs:
+        P - similarity matrix of points in the high-dimensional space
+        Q - similarity matrix of points in the low-dimensional map space
+
+    Output: Gradient of KL divergence expression
+    """
+
+    Q = q_matrix(Y)
+
+    for i in range(Y.shape[0]):
+        sum_j = 0
+        for j in range(Y.shape[0]):
+            sum_j += ((Y[i] - Y[j]) * (P[i, j] - Q[i, j]) * (1 + np.linalg.norm(Y[i] - Y[j] ** 2)) ** -1)
+
+    return 4 * sum_j
+
+# test
+grad_KL(P_ij, X)
 
 # Perform gradient descent
 
