8b311ffa9e3edb6ae542a1f6dd64dc4aeaa62e16
aashareddy14 <aashareddy14@gmail.com>
2021-04-20 12:03:37 -0400

    update implementations and simplify

--- tsne_2.py
+++ tsne_2.py
@@ -7,6 +7,7 @@ import scipy.linalg as la
 from keras.datasets.mnist import load_data
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy.spatial import distance
 
 iris = sns.load_dataset('iris')
 
@@ -14,23 +15,19 @@ iris = sns.load_dataset('iris')
 X = np.array(iris[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']])
 specs = np.array(iris['species'])
 
-def neg_squared_euc_dists(X):
-    """Compute matrix containing negative squared euclidean
-    distance for all pairs of points in input matrix X
-
-    # Arguments:
-        X: matrix of size NxD
-    # Returns:
-        NxN matrix D, with entry D_ij = negative squared
-        euclidean distance between rows X_i and X_j
+def squared_euc_dist(X):
+    """Calculate squared euclidean distance for all pairs in a data matrix X with d dimensions and n rows.
+    Output is a pairwise distance matrix D that is nxn.
     """
-    # Math? See https://stackoverflow.com/questions/37009647
-    sum_X = np.sum(np.square(X), 1)
-    D = np.add(np.add(-2 * np.dot(X, X.T), sum_X).T, sum_X)
-    return -D
+    D = distance.squareform(distance.pdist(X, 'sqeuclidean'))
+    return D
 
-def softmax(X):
-    """Take softmax of each row of matrix X."""
+
+def calc_prob_matrix(distances, sigmas):
+    """Convert a distances matrix to a matrix of probabilities."""
+    two_sig_sq = 2. * np.square(sigmas.reshape((-1, 1)))
+
+    X = distances / two_sig_sq
 
     # Subtract max for numerical stability
     e_x = np.exp(X - np.max(X, axis=1).reshape([-1, 1]))
@@ -42,14 +39,6 @@ def softmax(X):
     e_x += 1e-8  # numerical stability
 
     return e_x / e_x.sum(axis=1).reshape([-1, 1])
-
-def calc_prob_matrix(distances, sigmas=None):
-    """Convert a distances matrix to a matrix of probabilities."""
-    if sigmas is not None:
-        two_sig_sq = 2. * np.square(sigmas.reshape((-1, 1)))
-        return softmax(distances / two_sig_sq)
-    else:
-        return softmax(distances)
 
 
 def binary_search(eval_fn, target, tol=1e-10, max_iter=10000,
@@ -67,15 +56,15 @@ def binary_search(eval_fn, target, tol=1e-10, max_iter=10000,
         Float, best input value to function found during search.
     """
     for i in range(max_iter):
-        guess = (lower + upper) / 2.
-        val = eval_fn(guess)
+        mid = (lower + upper) / 2.
+        val = eval_fn(mid)
         if val > target:
-            upper = guess
+            upper = mid
         else:
-            lower = guess
+            lower = mid
         if np.abs(val - target) <= tol:
             break
-    return guess
+    return mid
 
 
 def perplexity(distances, sigmas):
@@ -102,38 +91,15 @@ def find_optimal_sigmas(distances, target_perplexity):
         sigmas.append(correct_sigma)
     return np.array(sigmas)
 
-def q_joint(Y):
-    """Given low-dimensional representations Y, compute
-    matrix of joint probabilities with entries q_ij."""
-    # Get the distances from every point to every other
-    distances = neg_squared_euc_dists(Y)
-    # Take the elementwise exponent
-    exp_distances = np.exp(distances)
-    # Fill diagonal with zeroes so q_ii = 0
-    np.fill_diagonal(exp_distances, 0.)
-    # Divide by the sum of the entire exponentiated matrix
-    return exp_distances / np.sum(exp_distances), None
-
-def calc_prob_matrix(distances, sigmas=None):
-    """Convert a distances matrix to a matrix of probabilities."""
-    if sigmas is not None:
-        two_sig_sq = 2. * np.square(sigmas.reshape((-1, 1)))
-        return softmax(distances / two_sig_sq)
-    else:
-        return softmax(distances)
-
-def p_conditional_to_joint(P):
-    """Given conditional probabilities matrix P, return
-    approximation of joint distribution probabilities."""
-    return (P + P.T) / (2. * P.shape[0])
 
 def q_tsne(Y):
     """t-SNE: Given low-dimensional representations Y, compute
     matrix of joint probabilities with entries q_ij."""
-    distances = neg_squared_euc_dists(Y)
+    distances = -squared_euc_dist(Y)
     inv_distances = np.power(1. - distances, -1)
     np.fill_diagonal(inv_distances, 0.)
     return inv_distances / np.sum(inv_distances), inv_distances
+
 
 def p_joint(X, target_perplexity):
     """Given a data matrix X, gives joint probabilities matrix.
@@ -144,24 +110,27 @@ def p_joint(X, target_perplexity):
         P: Matrix with entries p_ij = joint probabilities.
     """
     # Get the negative euclidian distances matrix for our data
-    distances = neg_squared_euc_dists(X)
+    distances = -squared_euc_dist(X)
     # Find optimal sigma for each row of this distances matrix
     sigmas = find_optimal_sigmas(distances, target_perplexity)
     # Calculate the probabilities based on these optimal sigmas
     p_conditional = calc_prob_matrix(distances, sigmas)
     # Go from conditional to joint probabilities matrix
-    P = p_conditional_to_joint(p_conditional)
+    P = (p_conditional + p_conditional.T) / (2. * p_conditional.shape[0])
     return P
 
 
-def tsne_grad(P, Q, Y, inv_distances):
+def tsne_grad(P, Q, Y):
     """Estimate the gradient of t-SNE cost with respect to Y."""
     pq_diff = P - Q
     pq_expanded = np.expand_dims(pq_diff, 2)
     y_diffs = np.expand_dims(Y, 1) - np.expand_dims(Y, 0)
 
+    # Get Q and distances (distances only used for t-SNE)
+    distances = q_tsne(Y)[1]
+
     # Expand our inv_distances matrix so can multiply by y_diffs
-    distances_expanded = np.expand_dims(inv_distances, 2)
+    distances_expanded = np.expand_dims(distances, 2)
 
     # Multiply this by inverse distances matrix
     y_diffs_wt = y_diffs * distances_expanded
@@ -170,8 +139,8 @@ def tsne_grad(P, Q, Y, inv_distances):
     grad = 4. * (pq_expanded * y_diffs_wt).sum(1)
     return grad
 
-def estimate_sne(X, y, P, rng, num_iters, q_fn, grad_fn, learning_rate,
-                 momentum, plot):
+
+def estimate_sne(X, num_iters=1000, q_fn=q_tsne, perplexity=30, learning_rate=10, momentum=0.9):
     """Estimates a SNE model.
 
     # Arguments
@@ -186,8 +155,12 @@ def estimate_sne(X, y, P, rng, num_iters, q_fn, grad_fn, learning_rate,
         Y: Matrix, low-dimensional representation of X.
     """
 
-    # Initialise our 2D representation
+    # Initialise our 2D representation, numpy RandomState for reproducibility
+    rng = np.random.RandomState(1)
     Y = rng.normal(0., 0.0001, [X.shape[0], 2])
+
+    # Obtain matrix of joint probabilities p_ij
+    P = p_joint(X, perplexity)
 
     # Initialise past values (used for momentum)
     if momentum:
@@ -200,7 +173,7 @@ def estimate_sne(X, y, P, rng, num_iters, q_fn, grad_fn, learning_rate,
         # Get Q and distances (distances only used for t-SNE)
         Q, distances = q_fn(Y)
         # Estimate gradients with respect to Y
-        grads = grad_fn(P, Q, Y, distances)
+        grads = tsne_grad(P, Q, Y)
 
         # Update Y
         Y = Y - learning_rate * grads
@@ -211,45 +184,13 @@ def estimate_sne(X, y, P, rng, num_iters, q_fn, grad_fn, learning_rate,
             Y_m1 = Y.copy()
 
         # Plot sometimes
-        #if plot and i % (num_iters / plot) == 0:
-         #   categorical_scatter_2d(Y, y, alpha=1.0, ms=6,
-          #                         show=True, figsize=(9, 6))
+        # if plot and i % (num_iters / plot) == 0:
+        #   categorical_scatter_2d(Y, y, alpha=1.0, ms=6,
+        #                         show=True, figsize=(9, 6))
 
     return Y
 
-# Set global parameters
-NUM_POINTS = 200            # Number of samples from MNIST
-CLASSES_TO_USE = [0, 1, 8]  # MNIST classes to use
-PERPLEXITY = 20
-SEED = 1                    # Random seed
-MOMENTUM = 0.9
-LEARNING_RATE = 10.
-NUM_ITERS = 1000            # Num iterations to train for
-#TSNE = False                # If False, Symmetric SNE
-NUM_PLOTS = 5               # Num. times to plot in training
 
-
-def main():
-    # numpy RandomState for reproducibility
-    rng = np.random.RandomState(SEED)
-
-    # Load the first NUM_POINTS 0's, 1's and 8's from MNIST
-    (n, d) = X.shape
-    y = np.random.randn(n, 2)
-
-    # Obtain matrix of joint probabilities p_ij
-    P = p_joint(X, PERPLEXITY)
-
-    # Fit SNE or t-SNE
-    Y = estimate_sne(X, y, P, rng,
-             num_iters=NUM_ITERS,
-             q_fn=q_tsne,
-             grad_fn=tsne_grad,
-             learning_rate=LEARNING_RATE,
-             momentum=MOMENTUM,
-             plot=NUM_PLOTS)
-    return Y
-
-yout = main()
+yout = estimate_sne(X)
 
 sns.scatterplot(yout[:,0], yout[:,1], hue = specs)
