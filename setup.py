16ae236ce8b48d25a8e65780dc795b48a78a196f
Madeleine Beckner <mebeckner@gmail.com>
2021-04-23 20:18:59 -0400

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="TSNE-reddy-beckner",
-    version="0.0.1",
+    version="0.0.2",
     author="Aasha Reddy, Madeleine Beckner",
     author_email="aashareddy14@gmail.com",
     description="Implementation of t-Distributed Stochastic Neighbor Embedding",
