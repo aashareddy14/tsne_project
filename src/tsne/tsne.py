cd553942ada5c1fa4c9bf25af05747cb73988901
Madeleine Beckner <40649770+mebeckner@users.noreply.github.com>
2021-04-24 16:32:24 -0400

    Update tsne.py

--- src/tsne/tsne.py
+++ src/tsne/tsne.py
@@ -9,31 +9,32 @@ import numba
 from numba import jit, int32, int64, float32, float64
 import pstats
 
-
-
-@jit(nopython=True)
-def p_ij(d_matrix, perplexity=30.0, tol=1e-6):
-    """
+@jit(nopython = True)
+def p_ij(d_matrix, perplexity = 40.0, tol = 1e-6):
+    '''
     Finds P_ij matrix using binary search to find value of sigma_i
 
-    Inputs: d_matrix- np.array of pairwise distance matrix, with a fixed perplexity
+    Inputs: 
+        d_matrix- np.array of pairwise distance matrix, with a fixed perplexity
+        perplexity: float chosen by user
+        tolerance: float chosen by user 
 
     Output: P-ij matrix
-    """
+    '''
 
     (n, d) = d_matrix.shape
 
     P = np.zeros((n, d), dtype=np.float64)
-    prec_sum = 0.0
-
+    prec_sum = 0.0    
+    
     # precision = 1/2sigma^2
     for i in range(n):
         prec_min = -np.inf
         prec_max = np.inf
         prec = 1.0
 
-        # implement binary search for optimal sigmas
-        for j in range(10):  # 10 binary search steps
+        # implement binary search for optimal sigmas 
+        for j in range(10): # 10 binary search steps
             sum_p = 0.0
             for k in range(d):
                 if k != i:
@@ -43,13 +44,14 @@ def p_ij(d_matrix, perplexity=30.0, tol=1e-6):
             sum_p_distribution = 0.0
 
             for k in range(d):
-                P[i, k] /= sum_p
+                P[i, k] /= (sum_p + 1e-8)
                 sum_p_distribution += d_matrix[i, k] * P[i, k]
 
             # Calculate entropy, H matrix
             H = np.log(sum_p) + prec * sum_p_distribution
             H_diff = H - np.log(perplexity)
 
+            
             # check if entropy is within tolerance
             if np.fabs(H_diff) <= tol:
                 break
@@ -72,58 +74,87 @@ def p_ij(d_matrix, perplexity=30.0, tol=1e-6):
     return P
 
 
-@jit(nopython=True)
+@jit(nopython = True)
 def squared_euc_dist(X):
-    """Calculate squared euclidean distance for all pairs in a data matrix X with d dimensions and n rows.
+    '''
+    Calculates squared euclidean distance for all pairs in a data matrix X with d dimensions and n rows. 
+    
+    Input: Matrix X
+       
     Output is a pairwise distance matrix D that is nxn.
-    """
-    norms = np.power(X, 2).sum(axis=1)
-    D = np.add(np.add(-2 * np.dot(X, X.T), norms).T, norms)
+    '''
+    
+    n = X.shape[0]
+    d = X.shape[1]
+    D = np.empty((n, n), dtype=np.float64)
+    
+    for i in range(n):
+        for j in range(n):
+            dist = 0.0
+            for k in range(d):
+                diff = X[i, k] - X[j, k]
+                dist += diff * diff
+            D[i, j] = dist
+        
+    return np.asarray(D)
 
-    return D
 
 
-@jit(nopython=True)
+
+@jit(nopython = True)
 def q_ij(Y):
-    """Calculate joint probabilities over all points given Y, the low-dimensional map of data points. (pg. 2585)"""
-
+    '''
+    Calculate joint probabilities over all points given Y, the low-dimensional map of data points. (pg. 2585)
+    
+    Input: Matrix Y
+     
+    Output: 
+    '''
+    
     numerator = np.power(1. + (squared_euc_dist(Y)), -1)
     Q = numerator / np.sum(numerator)
-
+    
     # q_i|i = 0
     np.fill_diagonal(Q, 0.)
-
+    
     return Q
 
-
-@jit(nopython=True)
+@jit(nopython = True)
 def grad_C(P, Q, Y):
-    """Estimate the gradient of t-SNE cost with respect to Y."""
-
+    '''
+    Estimate the gradient of t-SNE cost with respect to Y.
+    
+    Input:
+        P:
+        Q:
+        Y:
+    '''
+    
     pq_diff = np.expand_dims((P - Q), 2)
-
+    
     y_diff = np.expand_dims(Y, 1) - np.expand_dims(Y, 0)
-
+    
     y_dist = np.expand_dims(np.power(1 + squared_euc_dist(Y), -1), 2)
 
-    grad = 4. * (pq_diff * y_diff * y_dist).sum(axis=1)
-
+    grad = 4. * (pq_diff * y_diff * y_dist).sum(axis = 1)
+    
     return grad
 
 
-@jit(nopython=True)
-def tsne(X, num_iters=1000, perplexity=30, alpha=10, momentum=0.9):
-    """Calculate Y, the optimal low-dimensional representation of data matrix X using optimized TSNE.
-
-    Inputs:
-        X: data matrix
-        num_iters: number of iterations
+@jit(nopython = True)
+def TSNE(X, num_iters = 1000, perplexity = 40, alpha = 100, momentum = 0.8):
+    '''
+    Calculates Y, the optimal low-dimensional representation of data matrix X using optimized TSNE.
+    
+    Inputs: 
+        X: data matrix 
+        num_iters: number of iterations 
         perplexity: target perplexity for calculating optimal sigmas for P probability matrix
         alpha: learning rate
-        momentum: momentum to speed up gradient descent algorithm
-    """
+        momentum: momentum to speed up gradient descent algorithm 
+    '''
 
-    # Initialize Y
+    # Initialize Y 
     np.random.seed(0)
     Y = np.random.normal(0, 0.0001, size=(X.shape[0], 2))
     D = squared_euc_dist(X)
@@ -137,14 +168,41 @@ def tsne(X, num_iters=1000, perplexity=30, alpha=10, momentum=0.9):
 
     # gradient descent with momentum
     for i in range(num_iters):
+
         Q = q_ij(Y)
         grad = grad_C(P, Q, Y)
 
         # Update Y using momentum (pg. 2587)
-        Y = (Y - (alpha * grad)) + (momentum * (Y_tmin1 - Y_tmin2))
-
+        Y = (Y - (alpha * grad)) +  (momentum * (Y_tmin1 - Y_tmin2))
+  
         # update values of y_t-1 and y_t-2
         Y_tmin2 = Y_tmin1
         Y_tmin1 = Y
 
     return Y
+
+
+def TSNE_plot(Y, labels):
+    '''
+    Plots visualization of TSNE output by label class
+    
+    Inputs:
+        Y: two-dimensional matrix that's been passed through TSNE function
+        labels: array of labels that correspond to the data X clusters
+    '''
+    plt.figure(figsize=(9.6,7.2))
+    df = pd.DataFrame(data=np.c_[Y, labels], columns=("x", "y", "label"))
+    l = len(set(labels))
+    
+    if l <= 5:
+        sns.scatterplot(data = df, x = "x", y = "y", hue = "label", palette = sns.color_palette("husl", l)).set(xlabel=None, ylabel=None)
+    
+    elif l <= 20:
+        sns.scatterplot(data = df, x = "x", y = "y", hue = "label", palette = sns.color_palette("husl", l)).set(xlabel=None, ylabel=None)
+        plt.legend(bbox_to_anchor=(1.03, 1), borderaxespad=0)
+    
+    else:
+        sns.scatterplot(data = df, x = "x", y = "y", hue = "label", palette = sns.color_palette("husl", l)).set(xlabel=None, ylabel=None)
+        plt.legend(bbox_to_anchor=(1.03, 1), borderaxespad=0, ncol=2)
+
+    plt.show()
